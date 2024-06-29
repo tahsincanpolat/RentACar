4c166824dcce6d350a48f941a5dfabe52d75eb9e
tahsincanpolat <tahsincanpolat@gmail.com>
2024-06-29 12:47:00 +0300

    create frmGirisYap

---
+++ RentACar/RentACar/frmKullaniciGiris.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RentACar
+{
+    partial class frmKullaniciGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_TC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.btn_giris = new System.Windows.Forms.Button();
+            this.lnklb_kayit = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(43, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC Kimlik No:";
+            // 
+            // txt_TC
+            // 
+            this.txt_TC.Location = new System.Drawing.Point(46, 61);
+            this.txt_TC.Name = "txt_TC";
+            this.txt_TC.Size = new System.Drawing.Size(136, 20);
+            this.txt_TC.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(43, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Şifre:";
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Location = new System.Drawing.Point(46, 104);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(136, 20);
+            this.txt_sifre.TabIndex = 1;
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.BackColor = System.Drawing.Color.IndianRed;
+            this.btn_giris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_giris.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_giris.Location = new System.Drawing.Point(46, 148);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(136, 48);
+            this.btn_giris.TabIndex = 2;
+            this.btn_giris.Text = "Giriş";
+            this.btn_giris.UseVisualStyleBackColor = false;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // lnklb_kayit
+            // 
+            this.lnklb_kayit.AutoSize = true;
+            this.lnklb_kayit.Location = new System.Drawing.Point(95, 199);
+            this.lnklb_kayit.Name = "lnklb_kayit";
+            this.lnklb_kayit.Size = new System.Drawing.Size(43, 13);
+            this.lnklb_kayit.TabIndex = 3;
+            this.lnklb_kayit.TabStop = true;
+            this.lnklb_kayit.Text = "Kayıt Ol";
+            this.lnklb_kayit.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklb_kayit_LinkClicked);
+            // 
+            // frmKullaniciGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 254);
+            this.Controls.Add(this.lnklb_kayit);
+            this.Controls.Add(this.btn_giris);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_TC);
+            this.Controls.Add(this.label1);
+            this.Name = "frmKullaniciGiris";
+            this.Text = "Kullanıcı Girişi";
+            this.Load += new System.EventHandler(this.frmKullaniciGiris_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_TC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.LinkLabel lnklb_kayit;
+    }
+}
---
+++ RentACar/RentACar/frmKullaniciGiris.cs
@@ -0,0 +1,67 @@
+﻿using RentACar.ORM.Context;
+using RentACar.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentACar
+{
+    public partial class frmKullaniciGiris : Form
+    {
+        ProjectContext db = new ProjectContext();
+        string TC = "";
+        string sifre = "";
+        int id;
+        public frmKullaniciGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKullaniciGiris_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnklb_kayit_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmKullaniciKayit kullaniciKayit = new frmKullaniciKayit();
+            kullaniciKayit.Show();
+            
+        }
+
+        private void btn_giris_Click(object sender, EventArgs e)
+        {
+            List<Yoneticiler> ynt = new List<Yoneticiler>();
+            ynt = db.Yoneticilers.Where(y => y.TC == txt_TC.Text).ToList();
+
+            foreach (Yoneticiler yonetici in ynt)
+            {
+                TC = yonetici.TC.ToString();
+                sifre = yonetici.Sifre.ToString();
+                id = yonetici.ID;
+            }
+
+            if(TC == txt_TC.Text)
+            {
+                if(sifre == txt_sifre.Text)
+                {
+                    MessageBox.Show("Hoşgeldiniz");
+                }
+                else
+                {
+                    MessageBox.Show("Şifre Hatalı");
+                }
+            }
+            else
+            {
+                MessageBox.Show("TC Hatalı");
+            }
+        }
+    }
+}
