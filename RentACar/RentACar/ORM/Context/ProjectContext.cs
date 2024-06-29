56c5b1ae0899dee9029886d6b8e0cef99a3218c6
tahsincanpolat <tahsincanpolat@gmail.com>
2024-06-29 12:28:55 +0300

    entities, project context, migrations

---
+++ RentACar/RentACar/ORM/Context/ProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using RentACar.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.ORM.Context
+{
+    public class ProjectContext : DbContext
+    {
+        // Veritabanına bağlanma ve Code First Yöntemi ile Entitylerimizi tablo olarak veritabanına yollama.
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server=.;Database=RentACar;uid=sa;pwd=1453";
+        }
+        // Migration steps
+        // enable-migrations
+        // add-migration 'a'
+        // update-database
+
+        public DbSet<Arabalar> Arabalars { get; set; }
+        public DbSet<Yoneticiler> Yoneticilers { get; set; }
+        public DbSet<Kiralamalar> Kiralamalars { get; set; }
+    }
+}
---
+++ RentACar/RentACar/ORM/Entity/Kiralamalar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.ORM.Entity
+{
+    public class Kiralamalar : BaseEntity
+    {
+        public int ArabaID { get; set; }
+        public int YoneticiID { get; set; }
+
+        public virtual Arabalar Arabalar { get; set; }
+        public virtual Yoneticiler Yoneticiler { get; set; }
+
+        public DateTime AlisTarihi { get; set; }
+        public DateTime TeslimTarihi { get; set; }
+        public int? KiralamaSuresi { get; set; }
+        public double FaturaTutari { get; set; }
+    }
+}
